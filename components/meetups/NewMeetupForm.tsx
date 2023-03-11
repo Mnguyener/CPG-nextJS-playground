c6a00307bd7602b21d48af18c3437283a90fac95
mnguyener <michellehnguyen03@gmail.com>
2023-03-11 10:41:06 -0600

    added tutorial components and fixed them to typescript syntax

---
+++ components/meetups/NewMeetupForm.tsx
@@ -0,0 +1,62 @@
+import { useRef } from 'react';
+
+import Card from '../ui/Card';
+import classes from './NewMeetupForm.module.css';
+
+function NewMeetupForm(props: any) {
+  const titleInputRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+  const imageInputRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+  const addressInputRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+  const descriptionInputRef = useRef() as React.MutableRefObject<HTMLInputElement>;
+
+  function submitHandler(event: any) {
+    event.preventDefault();
+
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+      title: enteredTitle,
+      image: enteredImage,
+      address: enteredAddress,
+      description: enteredDescription,
+    };
+
+    props.onAddMeetup(meetupData);
+  }
+
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='title'>Meetup Title</label>
+          <input type='text' required id='title' ref={titleInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='image'>Meetup Image</label>
+          <input type='url' required id='image' ref={imageInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='address'>Address</label>
+          <input type='text' required id='address' ref={addressInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='description'>Description</label>
+          <textarea
+            id='description'
+            required
+            rows={5}
+            ref={descriptionInputRef.current.value} // not sure if this is correct
+          ></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default NewMeetupForm;
